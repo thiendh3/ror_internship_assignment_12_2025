b9352c56f6c09880b93c3a928e1c624462682471
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-15 08:24:08 +0700

    add callback to create notification after a relationship is created

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,16 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+
+  after_create_commit :create_notification
+
+  private
+    def create_notification
+      Notification.create(
+        recipient: followed,
+        actor: follower,
+        action: "followed",
+        notifiable: self
+      )
+    end
 end
