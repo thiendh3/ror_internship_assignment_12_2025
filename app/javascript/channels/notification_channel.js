648de1d43cebaa940f7071396c82b3f373be7056
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-15 09:02:34 +0700

    set up client side web socket logic

---
+++ app/javascript/channels/notification_channel.js
@@ -0,0 +1,54 @@
+import consumer from "channels/consumer";
+
+consumer.subscriptions.create("NotificationChannel", {
+	connected() {
+		console.log("Connected to NotificationChannel");
+	},
+
+	received(data) {
+		const badge = document.querySelector("#notification-count");
+		if (badge) {
+			const currentCount = parseInt(badge.innerText || 0);
+			badge.innerText = currentCount + 1;
+			badge.style.display = "inline-block";
+		}
+
+		const container = document.querySelector("#notifications-container");
+		const noNotifMessage = document.querySelector("#no-notifications");
+
+		if (noNotifMessage) noNotifMessage.remove();
+
+		const newNotificationHtml = `<li><a href="#">${data.actor_name} ${data.action} you</a></li>`;
+		if (container) {
+			container.insertAdjacentHTML("afterbegin", newNotificationHtml);
+		}
+	},
+});
+
+document.addEventListener("turbo:load", () => {
+	const toggle = document.querySelector("#notification-toggle");
+
+	if (toggle) {
+		toggle.addEventListener("click", () => {
+			const badge = document.querySelector("#notification-count");
+
+			if (badge && badge.style.display !== "none") {
+				badge.style.display = "none";
+				badge.innerText = "0";
+
+				fetch(toggle.dataset.url, {
+					method: "POST",
+					headers: {
+						"X-CSRF-Token": document.querySelector(
+							"[name='csrf-token']"
+						).content,
+						"Content-Type": "application/json",
+					},
+				}).then((response) => {
+					if (!response.ok)
+						console.error("Failed to mark notifications as read");
+				});
+			}
+		});
+	}
+});
